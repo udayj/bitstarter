32c9541c338c29be60b32b8c4079ee41af36529d
Uday Jhunjhunwala <jhunjhunwala.uday@gmail.com>
2013-07-06 21:56:20 +0530

    Index

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var fs=require('fs');
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  
+  var buffer=fs.readFileSync('index.html');
+  output=buffer.toString('utf-8')
+  response.send(output);
 });
 
 var port = process.env.PORT || 5000;
